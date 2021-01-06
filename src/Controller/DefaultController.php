5159586bb32c8e7d93de72915118c3948d2f7003
artem <artem.digi@gmail.com>
2021-01-06 21:04:50 +0700

    add tpl

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="mainpage")
+     */
+    public function index(): Response
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about(): Response
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'ABOUT',
+        ]);
+    }
+}
